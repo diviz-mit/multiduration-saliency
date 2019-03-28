a91c4cf69b70f90879e4230c53bfeaeb8349470b
Anelise Newman <apnewman@visiongpu30.csail.mit.edu>
2019-03-28 02:36:55 -0400

    function to sort out the losses

--- src/util.py
+++ src/util.py
@@ -1,5 +1,5 @@
 from multiduration_models import md_sem, sam_resnet_md
-from losses_keras2 import loss_wrapper, kl_time, cc_time, nss_time
+from losses_keras2 import loss_wrapper, kl_time, cc_time, nss_time, cc_match
 
 MODELS = {
     'md-sem': (md_sem, 'singlestream'),
@@ -7,9 +7,10 @@ MODELS = {
 }
 
 LOSSES = {
-    'kl': kl_time, 
-    'cc': cc_time,
-    'nss': nss_time,
+    'kl': (kl_time, 'heatmap'),
+    'cc': (cc_time, 'heatmap'),
+    'nss': (nss_time, 'fixmap'),
+    'ccmatch': (cc_match, 'heatmap')
 }
 
 def get_model_by_name(name): 
@@ -27,6 +28,35 @@ def get_loss_by_name(name, out_size):
     `name` so that keras can interpret it as a keras loss.
     """
     if name not in LOSSES: 
-        return name
+        return name, 'heatmap'
     else: 
-        return loss_wrapper(LOSSES[name], out_size)
+        loss, out_type = LOSSES[name]
+        loss = loss_wrapper(loss, out_size)
+        return loss, out_type
+
+def create_losses(loss_dict, out_size): 
+    """Given a dictionary that maps loss names to weights, returns loss functions and weights in the correct order. 
+
+    By convention, losses that take in a heatmap (as opposed to a fixmap) come first in the array of losses. This function enforces that convention.
+
+    This function looks up the correct loss function by name and outputs the correct functions, ordering, and weights to pass to the model/generator.
+    """
+    l_hm = []
+    l_hm_w = [] 
+    l_fm = []
+    l_fm_w = []
+    lstr = ""
+    for lname, wt in loss_dict.items(): 
+        loss, out_type = get_loss_by_name(lname, out_size)    
+        if out_type == 'heatmap': 
+            l_hm.append(loss)
+            l_hm_w.append(wt)
+        else: 
+            l_fm.append(loss)
+            l_fm_w.append(wt)
+        lstr += lname + str(wt)
+
+    l = l_hm + l_fm
+    lw = l_hm_w + l_fm_w
+    n_heatmaps = len(l_hm)
+    return l, lw, lstr, n_heatmaps    
